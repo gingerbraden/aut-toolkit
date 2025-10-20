7e3e8d5cf5eb9059500644065352bc460db13351
Jakub Krištof <kristof.jacobb@gmail.com>
2025-10-20 18:29:46 +0200

    feat(app): code refactor

--- lib/core/services/objectbox.dart
+++ lib/core/services/objectbox.dart
@@ -1,10 +1,8 @@
 import 'package:aut_toolkit/features/eating_habits/data/model/eating_habit_entity.dart';
 import 'package:aut_toolkit/objectbox.g.dart';
-import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
 class ObjectBox {
-  /// The Store of this app.
   late final Store store;
 
   late final Box<EatingHabitEntity> eatingHabitEntityBox;
@@ -13,7 +11,6 @@ class ObjectBox {
     eatingHabitEntityBox = Box<EatingHabitEntity>(store);
   }
 
-  /// Create an instance of ObjectBox to use throughout the app.
   static Future<ObjectBox> create() async {
     final docsDir = await getApplicationDocumentsDirectory();
     final store = await openStore(directory: '${docsDir.path}/objectbox');
